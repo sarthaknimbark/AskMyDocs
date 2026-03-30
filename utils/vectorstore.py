937d158b7b84d97172b90bbf2291a520194fc785
Sarthak Opash <sni.opash@gmail.com>
2026-03-30 16:35:23 +0530

    revert-back

--- utils/vectorstore.py
+++ utils/vectorstore.py
@@ -1,5 +1,5 @@
 from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 import streamlit as st
 
 

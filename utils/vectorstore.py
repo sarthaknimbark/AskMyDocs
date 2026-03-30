0a7122c0c4eee804b07c4453d346a3c81617b819
Sarthak Opash <sni.opash@gmail.com>
2026-03-30 16:34:41 +0530

    edited

--- utils/vectorstore.py
+++ utils/vectorstore.py
@@ -1,5 +1,5 @@
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 import streamlit as st
 
 
